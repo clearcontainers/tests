727c6caccd87f5a2d59b5153e9b954322053ab79
Damien Lespiau <damien.lespiau@intel.com>
2017-05-23 15:45:43 +0100

    ci: Always use # at the start of header lines
    It's quite usual to do that, so do it here as well.
    
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- .ci/install_cc_env_ubuntu.sh
+++ .ci/install_cc_env_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+#
 # Copyright (c) 2017 Intel Corporation
-
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 set -e
 
