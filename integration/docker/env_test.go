fafc5fd7a89cb9dda1bf2a5730948f729569b73e
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2017-07-31 12:41:21 +0000

    test: Check env in a container
    This will check the path, hostname and home of a container
    
    Fixes #294
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

---
+++ integration/docker/env_test.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2017 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package docker
+
+import (
+	. "github.com/clearcontainers/tests"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("env", func() {
+	var (
+		args []string
+		id   string
+	)
+
+	AfterEach(func() {
+		Expect(ContainerRemove(id)).To(BeTrue())
+		Expect(ContainerExists(id)).NotTo(BeTrue())
+	})
+
+	Describe("check env", func() {
+		Context("check that required env variables are set", func() {
+			It("should have path, hostname, home", func() {
+				id = randomDockerName()
+				hostname := "container"
+				args = []string{"run", "--name", id, "-h", hostname, Image, "env"}
+				stdout := runDockerCommand(0, args...)
+				Expect(stdout).To(ContainSubstring("PATH"))
+				Expect(stdout).To(ContainSubstring("HOME"))
+				Expect(stdout).To(ContainSubstring("HOSTNAME=" + hostname))
+			})
+		})
+	})
+})
