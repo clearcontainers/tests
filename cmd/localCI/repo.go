c4cdc82d36063e96a8d27d72b1e4da5625976fa4
Julio Montes <julio.montes@intel.com>
2017-08-08 11:01:14 -0500

    localCI: refactor repo.go
    this patch refactor repo.go in order to be able
    to test the master branch
    
    fixes #242
    
    Signed-off-by: Julio Montes <julio.montes@intel.com>

--- cmd/localCI/repo.go
+++ cmd/localCI/repo.go
@@ -19,7 +19,6 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -32,6 +31,9 @@ import (
 type Repo struct {
 	// URL is the url of the repository
 	URL string
+
+	// MasterBranch is the master branch of this repository
+	MasterBranch string
 
 	// PR is the pull request number
 	PR int
@@ -70,7 +72,7 @@ type Repo struct {
 	Language RepoLanguage
 
 	// CommentTrigger is the comment that must be present to trigger the test
-	CommentTrigger PullRequestComment
+	CommentTrigger RepoComment
 
 	// LogServer contains the information of the server where the logs must be placed
 	LogServer LogServer
@@ -92,9 +94,6 @@ type Repo struct {
 
 	// logger of the repository
 	logger *logrus.Entry
-
-	// projectSlug contains cvr domain, owner and repo name separated by '/'
-	projectSlug string
 }
 
 const (
@@ -124,13 +123,15 @@ func (r *Repo) setupCvr() error {
 		"Repo": r.URL,
 	})
 
-	// get the control version repository
-	r.cvr, err = newCvr(r.URL, r.Token)
-	r.logger.Debugf("control version repository: %#v", r.cvr)
+	// setup master branch
+	r.MasterBranch = strings.TrimSpace(r.MasterBranch)
+	if len(r.MasterBranch) == 0 {
+		r.MasterBranch = "master"
+	}
 
-	// set projectSlug
-	slug := []string{r.cvr.getDomain(), r.cvr.getOwner(), r.cvr.getDomain()}
-	r.projectSlug = strings.Join(slug, "/")
+	// get the control version repository
+	r.cvr, err = newCVR(r.URL, r.Token)
+	r.logger.Debugf("control version repository: %#v", r.cvr)
 
 	return err
 }
@@ -189,7 +190,7 @@ func (r *Repo) setupRefreshTime() error {
 }
 
 func (r *Repo) setupCommentTrigger() error {
-	if reflect.DeepEqual(r.CommentTrigger, PullRequestComment{}) {
+	if reflect.DeepEqual(r.CommentTrigger, RepoComment{}) {
 		return nil
 	}
 
@@ -226,7 +227,7 @@ func (r *Repo) setupEnvars() error {
 	// add environment variables
 	r.env = os.Environ()
 	r.env = append(r.env, defaultEnv...)
-	repoSlug := fmt.Sprintf("LOCALCI_REPO_SLUG=%s/%s", r.cvr.getOwner(), r.cvr.getRepo())
+	repoSlug := fmt.Sprintf("LOCALCI_REPO_SLUG=%s", r.cvr.getProjectSlug())
 	r.env = append(r.env, repoSlug)
 
 	return nil
@@ -261,70 +262,109 @@ func (r *Repo) setup() error {
 
 // loop to monitor the repository
 func (r *Repo) loop() {
-	var err error
-	var prsToTest map[string]*PullRequest
-	prsTested := make(map[string]*PullRequest)
+	revisionsTested := make(map[string]revision)
 
-	r.logger.Debugf("monitoring in a loop the repository: %#v", r)
+	prConfig := pullRequestConfig{
+		cvr:            r.cvr,
+		logger:         r.logger,
+		commentTrigger: r.CommentTrigger,
+		postOnFailure:  r.PostOnFailure,
+		postOnSuccess:  r.PostOnSuccess,
+	}
+
+	r.logger.Debugf("monitoring in a loop the repository: %+v", *r)
+
+	appendPullRequests := func(revisions *[]revision, prs []int) error {
+		for _, pr := range prs {
+			r.logger.Debugf("requesting pull request %d", pr)
+			pr, err := newPullRequest(pr, prConfig)
+			if err != nil {
+				return fmt.Errorf("failed to get pull request '%d' %s", pr, err)
+			}
+			*revisions = append(*revisions, pr)
+		}
+		return nil
+	}
 
 	for {
-		// if PR is not 0 then we have to monitor just one PR
-		if r.PR != 0 {
-			prsToTest = make(map[string]*PullRequest)
-			var pr *PullRequest
-			r.logger.Debugf("requesting single pull request %d", r.PR)
-			pr, err = r.cvr.getPullRequest(r.PR)
-			prsToTest[strconv.Itoa(r.PR)] = pr
-		} else {
-			r.logger.Debugf("requesting open pull requests")
-			prsToTest, err = r.cvr.getOpenPullRequests()
-		}
+		var revisionsToTest []revision
 
+		// append master branch
+		r.logger.Debugf("requesting master branch: %s", r.MasterBranch)
+		branch, err := newRepoBranch(r.MasterBranch, r.cvr, r.logger)
 		if err != nil {
-			r.logger.Errorf("failed to get pull requests: %s", err)
-			goto sleep
+			r.logger.Warn("failed to get master branch %s: %s", r.MasterBranch, err)
+		} else {
+			revisionsToTest = append(revisionsToTest, branch)
 		}
 
-		for number, prToTest := range prsToTest {
-			prTested := prsTested[number]
-			if prTested != nil {
-				if prToTest.Equal(*prTested) {
-					r.logger.Debugf("pull request was already tested: %+v", prToTest)
-					goto sleep
-				}
-				// checking if the old version of the PR is being tested
-				if prTested.BeingTested {
-					r.logger.Debugf("pull request is being tested: %+v", prTested)
-					goto sleep
-				}
+		// append pull requests
+		if r.PR != 0 {
+			// if PR is not 0 then we have to monitor just one PR
+			if err = appendPullRequests(&revisionsToTest, []int{r.PR}); err != nil {
+				r.logger.Warn("failed to append pull request %d", r.PR, err)
 			}
-
-			if err := r.testPullRequest(prToTest); err != nil {
-				r.logger.Errorf("failed to test pull request %+v: %s", prToTest, err)
-				goto sleep
+		} else {
+			// append open pull request
+			r.logger.Debugf("requesting open pull requests")
+			prs, err := r.cvr.getOpenPullRequests()
+			if err != nil {
+				r.logger.Warn("failed to get open pull requests: %s", err)
+			} else if err = appendPullRequests(&revisionsToTest, prs); err != nil {
+				r.logger.Warn("failed to append pull requests %+v: %s", prs, err)
 			}
-
-			// copy the PR that was tested
-			// FIXME: remove the PR's that were closed or merged
-			prsTested[number] = prToTest
 		}
 
-	sleep:
-		r.logger.Debugf("going to sleep")
+		r.logger.Debugf("testing revisions: %#v", revisionsToTest)
+		r.testRevisions(revisionsToTest, &revisionsTested)
+
+		r.logger.Debugf("going to sleep: %s", r.RefreshTime)
 		time.Sleep(r.refresh)
+	}
+}
+
+func (r *Repo) testRevisions(revisions []revision, revisionsTested *map[string]revision) {
+	for _, revision := range revisions {
+		tested, ok := (*revisionsTested)[revision.id()]
+		if ok {
+			// checking if the old version of the PR is being tested
+			if tested.isBeingTested() {
+				r.logger.Debugf("revision is being tested: %#v", tested)
+				continue
+			}
+			if revision.equal(tested) {
+				r.logger.Debugf("revision was already tested: %#v", revision)
+				continue
+			}
+		}
+
+		if err := r.testRevision(revision); err != nil {
+			r.logger.Errorf("failed to test revision '%+v' %s", revision, err)
+			continue
+		}
+
+		// copy the PR that was tested
+		// FIXME: remove the PR's that were closed or merged
+		(*revisionsTested)[revision.id()] = revision
 	}
 }
 
 // test the pull request specified in the configuration file
 // if pr does not exist an error is returned
 func (r *Repo) test() error {
-	r.logger.Debugf("testing the repository: %#v", r)
-
 	if r.PR == 0 {
 		return fmt.Errorf("Missing pull request number in configuration file")
 	}
 
-	pr, err := r.cvr.getPullRequest(r.PR)
+	prConfig := pullRequestConfig{
+		cvr:            r.cvr,
+		logger:         r.logger,
+		commentTrigger: r.CommentTrigger,
+		postOnFailure:  r.PostOnFailure,
+		postOnSuccess:  r.PostOnSuccess,
+	}
+
+	rev, err := newPullRequest(r.PR, prConfig)
 	if err != nil {
 		return fmt.Errorf("failed to get pull request %d %s", r.PR, err)
 	}
@@ -333,91 +373,34 @@ func (r *Repo) test() error {
 	// we are just testing one pull request
 	runTestsInParallel = false
 
-	return r.testPullRequest(pr)
+	return r.testRevision(rev)
 }
 
-// testPullRequest tests a specific pull request.
+// testRevision tests a specific revision
 // returns an error if the test fail
-func (r *Repo) testPullRequest(pr *PullRequest) error {
-	var err error
-
-	// before check if the PR can be tested we have to set the
-	// comment trigger
-	if len(r.CommentTrigger.Comment) != 0 {
-		pr.CommentTrigger, err = r.cvr.getLatestPullRequestComment(pr.Number, r.CommentTrigger)
-		if err != nil {
-			return fmt.Errorf("missing comment trigger in pull request %s", err)
-		}
-	}
-
-	// check if the PR can be tested
-	if err = pr.canBeTested(); err != nil {
-		r.logger.Debugf("pull request %d cannot be tested: %s", pr.Number, err)
-		return err
-	}
-
-	if r.whitelistUsers[0] == "@" {
-		found, err := r.cvr.isMember(pr.Author)
-		if err != nil {
-			return fmt.Errorf("unable to determinate if user '%s' is a member of the organization %s", pr.Author, err)
-		}
-
-		if !found {
-			return fmt.Errorf("user '%s' is not a member of the organization", pr.Author)
-		}
-	} else if r.whitelistUsers[0] != "*" {
-		found := false
-		for _, u := range r.whitelistUsers {
-			if u == pr.Author {
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			return fmt.Errorf("user '%s' is not part of the whitelist", pr.Author)
-		}
-	}
-
+func (r *Repo) testRevision(rev revision) error {
 	if !runTestsInParallel {
 		testLock.Lock()
 		defer testLock.Unlock()
 	}
 
-	// change the pr state before start the test
-	pr.BeingTested = true
-
-	testFunc := func() error {
-		err := r.runTest(pr)
-		pr.BeingTested = false
-		return err
-	}
-
 	if !runTestsInParallel {
-		return testFunc()
+		return r.runTest(rev)
 	}
 
 	go func() {
-		err := testFunc()
+		err := r.runTest(rev)
 		if err != nil {
-			r.logger.Errorf("failed to test pull request %#v: %s", pr, err)
+			r.logger.Errorf("failed to test revision %#v: %s", rev, err)
 		}
 	}()
 
 	return nil
 }
 
-// Steps to test a pull request:
-// - generate a new language environment to test the project
-// - download the pull request
-// - run stages (setup, run and teardown)
-// - if any stage fails then 'PostOnFailure' and run 'onFailure' commands
-// - if all is ok 'postOnSuccess' and run 'onSuccess' commands
-// - teardown the project language
-// - remove working directory
-// - copy logs to the server
-func (r *Repo) runTest(pr *PullRequest) error {
-	langEnv, err := r.Language.generateEnvironment(r.projectSlug)
+// runTest generate an environment and run the stages
+func (r *Repo) runTest(rev revision) error {
+	langEnv, err := r.Language.generateEnvironment(r.cvr.getProjectSlug())
 	if err != nil {
 		return err
 	}
@@ -435,107 +418,51 @@ func (r *Repo) runTest(pr *PullRequest) error {
 		}
 	}()
 
-	pr.WorkingDir = langEnv.workingDir
-
-	// clone the project
-	r.logger.Debugf("downloading pull request: %+v", *pr)
-	err = r.cvr.downloadPullRequest(pr.Number, pr.BranchName, langEnv.workingDir)
-	if err != nil {
+	// download the revision
+	if err = rev.download(langEnv.workingDir); err != nil {
 		return err
 	}
 
+	config := stageConfig{
+		logger:     r.logger,
+		workingDir: langEnv.workingDir,
+	}
+
 	// cleanup and set the log directory of the pull request
-	pr.LogDir = filepath.Join(r.LogDir, strconv.Itoa(pr.Number))
-	_ = os.RemoveAll(pr.LogDir)
-	if err = os.MkdirAll(pr.LogDir, logDirMode); err != nil {
+	config.logDir = filepath.Join(r.LogDir, rev.logDirName())
+	_ = os.RemoveAll(config.logDir)
+	if err = os.MkdirAll(config.logDir, logDirMode); err != nil {
 		return err
 	}
 
 	// set environment variables
-	pr.Env = r.env
+	config.env = r.env
 
 	// appends language environment variables
 	if len(langEnv.env) > 0 {
-		pr.Env = append(pr.Env, langEnv.env...)
+		config.env = append(config.env, langEnv.env...)
 	}
-
-	// appends other environment variables
-	prNumber := fmt.Sprintf("LOCALCI_PR_NUMBER=%d", pr.Number)
-	pr.Env = append(pr.Env, prNumber)
 
 	// copy logs to server if we have an IP address
 	if len(r.LogServer.IP) != 0 {
 		defer func() {
-			err = r.LogServer.copy(pr.LogDir)
+			err = r.LogServer.copy(config.logDir)
 			if err != nil {
-				r.logger.Errorf("failed to copy log dir %s to server %+v", pr.LogDir, r.LogServer)
+				r.logger.Errorf("failed to copy log dir %s to server %+v", config.logDir, r.LogServer)
 			}
 		}()
 	}
 
-	// run stages
-	return r.runStages(pr)
-}
+	r.logger.Debugf("stage config: %+v", config)
 
-func (r *Repo) runStages(pr *PullRequest) error {
-	var err error
-
-	stages := []struct {
-		name     string
-		commands []string
-	}{
-		{name: "setup", commands: r.Setup},
-		{name: "run", commands: r.Run},
-		{name: "teardown", commands: r.Teardown},
+	stages := map[string]stage{
+		"setup":     stage{name: "setup", commands: r.Setup},
+		"run":       stage{name: "run", commands: r.Run},
+		"teardown":  stage{name: "teardown", commands: r.Teardown},
+		"onSuccess": stage{name: "onSuccess", commands: r.OnSuccess},
+		"onFailure": stage{name: "onFailure", commands: r.OnFailure},
 	}
 
-	r.logger.Debugf("testing pull request: %+v", *pr)
-
-	for _, s := range stages {
-		if len(s.commands) == 0 {
-			r.logger.Debugf("there are not commands for the stage '%s'", s.name)
-			continue
-		}
-
-		r.logger.Debugf("running stage: %+v", s)
-
-		if err = pr.runStage(s.name, s.commands); err != nil {
-			r.logger.Errorf("failed to run stage '%s': %s", s.name, err)
-			if len(r.PostOnFailure) != 0 {
-				if err = r.cvr.createComment(pr.Number, r.PostOnFailure); err != nil {
-					r.logger.Errorf("failed to create comment '%s' on pull request %d", r.PostOnFailure, pr.Number)
-				}
-			}
-
-			r.logger.Debugf("running 'onFailure' stage")
-
-			if e := pr.runStage("onFailure", r.OnFailure); e != nil {
-				r.logger.Errorf("failed to run 'onFailure' stage: %s", err)
-			}
-
-			return err
-		}
-	}
-
-	// check if there are commands to run onSuccess
-	if len(r.OnSuccess) != 0 {
-		defer func() {
-			r.logger.Debugf("running 'onSuccess' stage")
-
-			// run 'onSuccess' stage
-			if err := pr.runStage("onSuccess", r.OnSuccess); err != nil {
-				r.logger.Errorf("failed to run 'onSuccess' stage: %s", err)
-			}
-		}()
-	}
-
-	// check if there is a comment to create on success
-	if len(r.PostOnSuccess) != 0 {
-		err = r.cvr.createComment(pr.Number, r.PostOnSuccess)
-		if err != nil {
-			return fmt.Errorf("failed to create comment in pull request %d %s", pr.Number, err)
-		}
-	}
-
-	return nil
+	// run test
+	return rev.test(config, stages)
 }
