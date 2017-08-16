917f4c6d178954ca22d2532623c27792f7fe216b
Julio Montes <julio.montes@intel.com>
2017-08-16 10:12:02 -0500

    tests: fix IsVMRunning function
    to avoid crashes in IsVMRunning function we shouldn't use
    the reference to FileInfo that filepath.Walk provides
    
    fixes #396
    
    Signed-off-by: Julio Montes <julio.montes@intel.com>

--- vm.go
+++ vm.go
@@ -29,7 +29,16 @@ var errFound = errors.New("found")
 // IsVMRunning looks in /proc for a hypervisor process that contains
 // the containerID in its command line
 func IsVMRunning(containerID string) bool {
-	err := filepath.Walk(procPath, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(procPath, func(path string, _ os.FileInfo, _ error) error {
+		if path == "" {
+			return filepath.SkipDir
+		}
+
+		info, err := os.Stat(path)
+		if err != nil {
+			return filepath.SkipDir
+		}
+
 		if !info.IsDir() {
 			return filepath.SkipDir
 		}
