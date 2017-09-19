7026d4dfb83a30ac676ab21d571e0abbe98a51d4
Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>
2017-09-19 06:23:56 -0500

    metrics: network: Fix/simplify CPU test
    This commit allows to work with runc runtime,
    furthermore it removes redundant/deprecated code
    and it adds more comments to the code.
    
    Another addition is to enable "exit immediately" if a
    pipeline return non-zero status.
    
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>

--- metrics/network/network-metrics-cpu-consumption.sh
+++ metrics/network/network-metrics-cpu-consumption.sh
@@ -17,28 +17,33 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 # Description:
-#  Measures cpu % consumption using an inter (docker<->docker) 
-#  network bandwidth using iperf2
+# This metrics test measures the cpu % consumption while it is running a
+# network/bandwidth workload using iperf tool in an interconnection
+# container-client <------> container-server.
+
+set -e
 
 SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
-
-source "${SCRIPT_PATH}/lib/network-test-common.bash"
+source "${SCRIPT_PATH}/lib/network-common.bash"
+source "${SCRIPT_PATH}/../lib/common.bash"
+TEST_NAME="network cpu consumption"
 
 # Set QEMU_PATH unless it's already set
 QEMU_PATH=${QEMU_PATH:-$(get_qemu_path)}
 
-# This script will perform all the measurements using a local setup
-
 # Measures cpu % consumption while running bandwidth measurements
-# using iperf2
-
-function cpu_consumption {
-	# Port number where the server will run
-	local port=5001:5001
+# using iperf2.
+function cpu_consumption() {
+	# Port number where the server will run.
+	# Server configuration, a container will work as a server, for this
+	# it will run iperf in server mode, by default, the iperf client connects
+	# to the iperf server on the TCP port 5001, for that reason the container
+	# makes port forwarding to 5001.
+	local port="5001:5001"
 	# Using this image as iperf is not working
 	# see (https://github.com/01org/cc-oci-runtime/issues/152)
 	# Image name
-	local image=gabyct/network
+	local image="gabyct/network"
 	# Total measurement time (seconds)
 	# This is required in order to reduce standard deviation
 	local total_time=16
@@ -47,19 +52,23 @@ function cpu_consumption {
 	# have server and client running for sometime and we
 	# need to avoid to measure at the beginning of the running
 	local middle_time=8
-	# Name of the containers
-	local server_name="network-server"
-	local client_name="network-client"
 	# Arguments to run the client
 	local extra_args="-d"
 
+	# Initialize/clean environment
 	init_env
-	setup
+
 	local server_command="iperf -p ${port} -s"
-	local server_address=$(start_server "$server_name" "$image" "$server_command")
+	local server_address=$(start_server "$image" "$server_command")
+
+	# Verify server IP address
+	if [ -z "$server_address" ];then
+		clean_env
+		die "server: ip address no found"
+	fi
 
 	local client_command="iperf -c ${server_address} -t ${total_time}"
-	start_client "$extra_args" "$client_name" "$image" "$client_command" > /dev/null
+	start_client "$image" "$client_command" "$extra_args"
 
 	# Measurement after client and server are more stable
 	echo >&2 "WARNING: sleeping for $middle_time seconds in order to have server and client stable"
@@ -75,16 +84,15 @@ function cpu_consumption {
 		die "Unknown runtime: $RUNTIME"
 	fi
 
-	qemu_pids=$(pidof ${QEMU_PATH})
 	local total_cpu_consumption=$(ps --no-headers -o %cpu \
 		-p $(pidof ${process}) | awk '{ total+= $1 } END { print total/NR }')
-	echo "The cpu % consumption is : $total_cpu_consumption"
+	echo "CPU % consumption: $total_cpu_consumption"
 
-	save_results "network metrics cpu consumption" "" \
+	save_results "$TEST_NAME" "" \
 		"$total_cpu_consumption" "%"
 
-	clean_environment "$server_name"
-	$DOCKER_EXE rm -f ${client_name} > /dev/null
+	clean_env
+	echo "Finish"
 }
 
 cpu_consumption
