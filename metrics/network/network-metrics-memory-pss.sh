0cd09fe8887305c7cc2c5123c0d3c476d13a470a
Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>
2017-09-20 09:52:48 -0500

    metrics: network: Fix/simplify memory-iperf test
    This commit allows to work with runc runtime, also it removes
    redundant/deprecated source code according to new changes in
    network common lib.
    
    Furthermore it fixes the information extraction in order
    to get correct measurements about PSS memory.
    
    Another additions is to enable "exit immediately" if a
    pipeline return non-zero status.
    
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>

--- metrics/network/network-metrics-memory-pss.sh
+++ metrics/network/network-metrics-memory-pss.sh
@@ -17,28 +17,27 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 # Description:
-#  Measures Proportional Set Size memory while running an 
-#  inter (docker<->docker) network bandwidth using iperf2
+# This metrics test measures Proportional Set Size memory while it is running
+# a network bandwidth workload using iperf2 tool in an interconnection
+# container-client <----> container-server.
+
+set -e
 
 SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
-
-source "${SCRIPT_PATH}/lib/network-test-common.bash"
+source "${SCRIPT_PATH}/lib/network-common.bash"
+source "${SCRIPT_PATH}/../lib/common.bash"
+TEST_NAME="network metrics memory pss"
 
 # Set QEMU_PATH unless it's already set
 QEMU_PATH=${QEMU_PATH:-$(get_qemu_path)}
 
-# This script will perform all the measurements using a local setup
-
-# Measures PSS memory while running bandwidth measurements
-# using iperf2
-
-function pss_memory {
+function pss_memory() {
 	# Port number where the server will run
-	local port=5001:5001
+	local port="5001:5001"
 	# Using this image as iperf is not working
 	# see (https://github.com/01org/cc-oci-runtime/issues/152)
 	# Image name
-	local image=gabyct/network
+	local image="gabyct/network"
 	# Total measurement time (seconds)
 	# This is required in order to reduce standard deviation
 	local total_time=10
@@ -47,32 +46,42 @@ function pss_memory {
 	# have server and client running for sometime and we
 	# need to avoid to measure at the beginning of the running
 	local middle_time=5
-	# Name of the containers
-	local server_name="network-server"
-	local client_name="network-client"
 	# Arguments to run the client
 	local extra_args="-d"
 
-	setup
+	# Initialize/clean environment
+	init_env
+
 	local server_command="iperf -p ${port} -s"
-	local server_address=$(start_server "$server_name" "$image" "$server_command")
+	local server_address=$(start_server "$image" "$server_command")
 
 	local client_command="iperf -c ${server_address} -t ${total_time}"
-	start_client "$extra_args" "$client_name" "$image" "$client_command" > /dev/null
+	start_client "$image" "$client_command" "$extra_args"
 
 	# Measurement after client and server are more stable
 	echo >&2 "WARNING: sleeping for $middle_time seconds in order to have server and client stable"
 	sleep ${middle_time}
+
+	# Check the runtime in order to determine which process will
+        # be measured about PSS memory consumption.
+        if [ "$RUNTIME" == "runc" ]; then
+                process="iperf"
+        elif [ "$RUNTIME" == "cor" ] || [ "$RUNTIME" == "cc-runtime" ]; then
+                process="$QEMU_PATH"
+        else
+                die "Unknown runtime: $RUNTIME"
+        fi
+
 	local memory_command="smem --no-header -c pss"
-	${memory_command} -P ${QEMU_PATH} > "$result"
+	result=$(${memory_command} -P ^${process})
 
-	local total_pss_memory=$(awk '{ total += $1 } END { print total/NR }' "$result")
-	echo "The PSS memory is : $total_pss_memory Kb"
+	local total_pss_memory=$(echo "$result" | awk '{ total += $1 } END { print total/NR }')
+	echo "PSS memory: $total_pss_memory Kb"
 
-	save_results "network metrics memory" "pss" "$total_pss_memory" "kb"
+	save_results "$TEST_NAME" "pss" "$total_pss_memory" "kb"
 
-	clean_environment "$server_name"
-	$DOCKER_EXE rm -f ${client_name} > /dev/null
+	clean_env
+	echo "Finish"
 }
 
 pss_memory
