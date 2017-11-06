4cec1eb73aa8b97358786dec18207c8534618815
Julio Montes <julio.montes@intel.com>
2017-11-06 10:16:48 -0600

    integration/docker: add hot plug test
    run a container with --device option and check if the
    device is visible inside the container
    
    fixes #675
    
    Signed-off-by: Julio Montes <julio.montes@intel.com>

--- integration/docker/run_test.go
+++ integration/docker/run_test.go
@@ -16,6 +16,9 @@ package docker
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
 
 	. "github.com/clearcontainers/tests"
 	. "github.com/onsi/ginkgo"
@@ -96,4 +99,92 @@ var _ = Describe("run", func() {
 		Entry("in background and interactive", "-di", "Up"),
 		Entry("in background, interactive and with a tty", "-dit", "Up"),
 	)
+})
+
+// creates a new disk file using 'dd' command, returns the path to disk file and
+// its loop device representation
+func createLoopDevice() (string, string, error) {
+	f, err := ioutil.TempFile("", "dd")
+	if err != nil {
+		return "", "", err
+	}
+	defer f.Close()
+
+	// create disk file
+	ddArgs := []string{"if=/dev/zero", fmt.Sprintf("of=%s", f.Name()), "count=1", "bs=5M"}
+	ddCmd := NewCommand("dd", ddArgs...)
+	if _, stderr, exitCode := ddCmd.Run(); exitCode != 0 {
+		return "", "", fmt.Errorf("%s", stderr)
+	}
+
+	// partitioning disk file
+	fdiskArgs := []string{"-c", fmt.Sprintf(`printf "g\nn\n\n\n\nw\n" | fdisk %s`, f.Name())}
+	fdiskCmd := NewCommand("bash", fdiskArgs...)
+	if _, stderr, exitCode := fdiskCmd.Run(); exitCode != 0 {
+		return "", "", fmt.Errorf("%s", stderr)
+	}
+
+	// create loop device
+	partxArgs := []string{"-av", f.Name()}
+	partxCmd := NewCommand("partx", partxArgs...)
+	stdout, stderr, exitCode := partxCmd.Run()
+	if exitCode != 0 {
+		return "", "", fmt.Errorf("%s", stderr)
+	}
+	partxRegexp := regexp.MustCompile("/dev/loop[[:digit:]]+")
+	loopFile := partxRegexp.Find([]byte(stdout))
+	if len(loopFile) == 0 {
+		return "", "", fmt.Errorf("failed to create loop device")
+	}
+
+	return f.Name(), string(loopFile), nil
+}
+
+func deleteLoopDevice(loopFile string) error {
+	partxArgs := []string{"-d", loopFile}
+	partxCmd := NewCommand("partx", partxArgs...)
+	_, stderr, exitCode := partxCmd.Run()
+	if exitCode != 0 {
+		return fmt.Errorf("%s", stderr)
+	}
+
+	return nil
+}
+
+var _ = Describe("run", func() {
+	var (
+		err        error
+		diskFile   string
+		loopFile   string
+		dockerArgs []string
+		id         string
+	)
+
+	BeforeEach(func() {
+		if os.Getuid() != 0 {
+			Skip("only root user can create loop devices")
+		}
+		id = RandID(30)
+		diskFile, loopFile, err = createLoopDevice()
+		Expect(err).ToNot(HaveOccurred())
+		dockerArgs = []string{"--rm", "--name", id, "--device", loopFile,
+			Image, "stat", loopFile}
+	})
+
+	AfterEach(func() {
+		Expect(ExistDockerContainer(id)).NotTo(BeTrue())
+		err = deleteLoopDevice(loopFile)
+		Expect(err).ToNot(HaveOccurred())
+		err = os.Remove(diskFile)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	Context("hot plug a block device", func() {
+		It("should be attached", func() {
+			//FIXME
+			//Skip("Issue: https://github.com/clearcontainers/runtime/issues/789")
+			_, _, exitCode := DockerRun(dockerArgs...)
+			Expect(exitCode).To(BeZero())
+		})
+	})
 })
