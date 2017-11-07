386047592bd1baa886cbb81c2867d5b95e1d4bb4
Graham Whaley <graham.whaley@intel.com>
2017-11-07 15:42:36 +0000

    CI: Jenkins: Note why we need CI=true
    We need CI=true to get the coverage tools to function, so let's
    note that in the file comments.
    
    Signed-off-by: Graham Whaley <graham.whaley@intel.com>

--- .ci/jenkins_job_build.sh
+++ .ci/jenkins_job_build.sh
@@ -37,6 +37,8 @@ mkdir -p ${GOPATH}
 # Export all environment variables needed.
 export GOROOT="/usr/local/go"
 export PATH=${GOPATH}/bin:/usr/local/go/bin:/usr/sbin:${PATH}
+
+# We need to set CI in order to enable proper coverage tool operation
 export CI=true
 
 # Download and build goveralls binary in case we need to submit the code
