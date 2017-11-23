22a4661bce589007ff9cbb5b4a8b826dfa1b0d9e
Graham whaley <graham.whaley@intel.com>
2017-11-23 14:22:01 +0000

    metrics: common: warn if we have unexpected processes
    If we find we have an unexpected hypervisor or shim process
    in the system, warn about it, show some info about the process,
    kill it, and continue.
    
    There are a number of little changes in here:
     - Do not use 'return 1' in the sub-funcs, as some of the scripts
       that invoke these funcs 'set -e', and thus die at that point,
       which is not the exepected behaviour.
     - use sudo with the killall, as if you are running the metrics
       as a non-priv user (the recomended method), then you will not
       have the privs to kill the hypervisor or the shim, and you end
       up with an odd 'process not found' warning in your logs, and
       don't actually kill the processes off
    
    Fixes: #748
    
    Signed-off-by: Graham whaley <graham.whaley@intel.com>

--- metrics/lib/common.bash
+++ metrics/lib/common.bash
@@ -227,9 +227,9 @@ function runtime_docker(){
 function check_active_process() {
 	process=$1
 	if pgrep -f "$process" > /dev/null; then
-		return 1
+		echo "1"
 	else
-		return 0
+		echo "0"
 	fi
 }
 
@@ -239,9 +239,21 @@ function check_active_process() {
 function kill_processes_before_start() {
 	DOCKER_PROCS=$(${DOCKER_EXE} ps -q)
 	[[ -n "${DOCKER_PROCS}" ]] && "${DOCKER_EXE}" kill ${DOCKER_PROCS}
+
 	HYPERVISOR_PATH="$(get_qemu_path)"
-	check_active_process "$HYPERVISOR_PATH" || killall "$HYPERVISOR_PATH"
-	check_active_process "$CC_SHIM_PATH" || killall "$CC_SHIM_PATH"
+	result=$(check_active_process "$HYPERVISOR_PATH")
+	if [[ $result -ne 0 ]]; then
+		warning "Found unexpected ${HYPERVISOR_PATH} processes present"
+		pgrep -a -f "$HYPERVISOR_PATH"
+		sudo killall "$HYPERVISOR_PATH"
+	fi
+
+	result=$(check_active_process "$CC_SHIM_PATH")
+	if [[ $result -ne 0 ]]; then
+		warning "Found unexpected ${CC_SHIM_PATH} processes present"
+		pgrep -a -f "$CC_SHIM_PATH"
+		sudo killall "$CC_SHIM_PATH"
+	fi
 }
 
 # Generate a random name - generally used when creating containers, but can
