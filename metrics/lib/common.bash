c44132bef2ec8d566bd9bc0f4e611c5dd7aa5fca
Graham whaley <graham.whaley@intel.com>
2017-12-11 11:20:27 +0000

    metrics: common: do not fail if pgrep fails
    We use pgrep to report diagnostics on errant processes,
    but sometimes those processes have gone by the time we
    go looking for them. In that case pgrep fails, but is non
    fatal. Similarly for the subsequent killall call.
    Code it as so.
    
    Fixes: #787
    
    Signed-off-by: Graham whaley <graham.whaley@intel.com>

--- metrics/lib/common.bash
+++ metrics/lib/common.bash
@@ -267,15 +267,17 @@ function kill_processes_before_start() {
 	result=$(check_active_process "$HYPERVISOR_PATH")
 	if [[ $result -ne 0 ]]; then
 		warning "Found unexpected ${HYPERVISOR_PATH} processes present"
-		pgrep -a -f "$HYPERVISOR_PATH"
-		sudo killall "$HYPERVISOR_PATH"
+		# Sometimes we race and the process has gone by the time we list
+		# it - so make a pgrep fail non-fatal
+		pgrep -a -f "$HYPERVISOR_PATH" || true
+		sudo killall "$HYPERVISOR_PATH" || true
 	fi
 
 	result=$(check_active_process "$CC_SHIM_PATH")
 	if [[ $result -ne 0 ]]; then
 		warning "Found unexpected ${CC_SHIM_PATH} processes present"
-		pgrep -a -f "$CC_SHIM_PATH"
-		sudo killall "$CC_SHIM_PATH"
+		pgrep -a -f "$CC_SHIM_PATH" || true
+		sudo killall "$CC_SHIM_PATH" || true
 	fi
 }
 
