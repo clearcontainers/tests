a5740673ef2aa20f88af2c3e67bc4380d37d83eb
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2018-02-13 11:51:26 -0600

    ci: jenkins: fix path to manage_ctr_mgr.sh
    Fix path to manage_ctr_mgr.sh
    
    Fixes: #907
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- .ci/jenkins_job_build.sh
+++ .ci/jenkins_job_build.sh
@@ -82,7 +82,7 @@ fi
 # Make sure runc is default runtime.
 # This is needed in case a new image creation.
 # See https://github.com/clearcontainers/osbuilder/issues/8
-"${GOPATH}/${tests_repo}/cmd/container-manager/manage_ctr_mgr.sh" docker configure -r runc  -f || true
+"${GOPATH}/src/${tests_repo}/cmd/container-manager/manage_ctr_mgr.sh" docker configure -r runc -f
 
 # Call the repo-specific setup script.
 #
